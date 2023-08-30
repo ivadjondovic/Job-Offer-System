6b61a7a01f0a44e3f8b5065693bcfb8badf526d6
Iva Đondović <djondovic.ra14.2018@uns.ac.rs>
2023-08-30 17:22:53 +0200

    Initial project commit

---
+++ Job-Offer-System/src/main/java/com/example/JobOfferSystem/JobOfferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.JobOfferSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferSystemApplication.class, args);
+	}
+
+}
---
+++ Job-Offer-System/src/test/java/com/example/JobOfferSystem/JobOfferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JobOfferSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
